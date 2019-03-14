78c6d5fa50fe4badc33a2d9036bd07ea1644f7e9
Niki Stavrou <nikist95@gmail.com>
2019-03-14 15:27:21 +0100

    first commit

---
+++ R/prepare_logo.R
@@ -0,0 +1,11 @@
+library(magick)
+library(rsvg)
+
+logo <- image_read_svg('https://upload.wikimedia.org/wikipedia/commons/b/b2/Eurocontrol_logo_2010.svg', width = 400)
+print(logo)
+
+ectl_logo <- image_scale(logo, "80")
+print(ectl_logo)
+
+image_write(ectl_logo, path = "C:/LocalFiles/RProjects/ectrlplot/ectl_logo.png", format = "png")
+#image_write(logo, path = "C:/LocalFiles/RProjects/bookdown/images/logo.png", format = "png")
