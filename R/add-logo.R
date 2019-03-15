988678e1f015c3033ad33b1171b2796ed6eec217
Niki Stavrou <nikist95@gmail.com>
2019-03-15 16:56:47 +0100

    pushes repo to Github

--- R/add-logo.R
+++ R/add-logo.R
@@ -1,25 +1,26 @@
-save_plot <- function (plot_grid, width, height, save_filepath) {
-  grid::grid.draw(plot_grid)
-  #save it
-  ggplot2::ggsave(filename = save_filepath,
-                  plot=plot_grid, width=(width/72), height=(height/72),  bg="white")
+save_plot <- function(plot_grid, width, height, save_filepath) {
+    grid::grid.draw(plot_grid)
+    # save it
+    ggplot2::ggsave(filename = save_filepath,
+                    plot = plot_grid,
+                    width = (width/72),
+                    height = (height/72),
+                    bg = "white")
 }
 
-#Left align text
-left_align <- function(plot_name, pieces){
-  grob <- ggplot2::ggplotGrob(plot_name)
-  n <- length(pieces)
-  grob$layout$l[grob$layout$name %in% pieces] <- 2
-  return(grob)
+# Left align text
+left_align <- function(plot_name, pieces) {
+    grob <- ggplot2::ggplotGrob(plot_name)
+    n <- length(pieces)
+    grob$layout$l[grob$layout$name %in% pieces] <- 2
+    return(grob)
 }
 
-create_footer <- function (source_name, logo_image_path) {
-  #Make the footer
-  footer <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.1, "npc")),
-                           grid::textGrob(source_name,
-                                          x = 0.004, hjust = 0, gp = grid::gpar(fontsize=16)),
-                           grid::rasterGrob(png::readPNG(logo_image_path), x = 0.944))
-  return(footer)
+create_footer <- function(source_name, logo_image_path) {
+    # Make the footer
+    footer <- grid::grobTree(grid::linesGrob(x = grid::unit(c(0, 1), "npc"), y = grid::unit(1.1, "npc")), grid::textGrob(source_name, x = 0.004,
+        hjust = 0, gp = grid::gpar(fontsize = 16)), grid::rasterGrob(png::readPNG(logo_image_path), x = 0.944))
+    return(footer)
 
 }
 
@@ -39,31 +40,24 @@ create_footer <- function (source_name, logo_image_path) {
 #' @keywords add_logo
 #' @examples
 #' add_logo(plot_name = myplot,
-#' source = "The source for my data",
-#' save_filepath = "filename_that_my_plot_should_be_saved_to-nc.png",
+#' source = 'The source for my data',
+#' save_filepath = 'filename_that_my_plot_should_be_saved_to-nc.png',
 #' width_pixels = 640,
 #' height_pixels = 450,
-#' logo_image_path = "logo_image_filepath.png"
+#' logo_image_path = 'logo_image_filepath.png'
 #' )
 #'
 #' @export
-add_logo <- function(plot_name,
-                          source_name,
-                          save_filepath=file.path(Sys.getenv("TMPDIR"), "tmp-nc.png"),
-                          width_pixels=640,
-                          height_pixels=450,
-                          logo_image_path = file.path(system.file("data", package = 'ectrlplot'),"ectl_logo.png")) {
+add_logo <- function(plot_name, source_name, save_filepath = file.path(Sys.getenv("TMPDIR"), "tmp-nc.png"), width_pixels = 640, height_pixels = 450,
+    logo_image_path = file.path(system.file("data", package = "ectrlplot"), "ectl_logo.png")) {
 
-  footer <- create_footer(source_name, logo_image_path)
+    footer <- create_footer(source_name, logo_image_path)
 
-  #Draw your left-aligned grid
-  plot_left_aligned <- left_align(plot_name, c("subtitle", "title", "caption"))
-  plot_grid <- ggpubr::ggarrange(plot_left_aligned, footer,
-                                 ncol = 1, nrow = 2,
-                                 heights = c(1, 0.045/(height_pixels/450)))
-  ## print(paste("Saving to", save_filepath))
-  save_plot(plot_grid, width_pixels, height_pixels, save_filepath)
-  ## Return (invisibly) a copy of the graph. Can be assigned to a
-  ## variable or silently ignored.
-  invisible(plot_grid)
+    # Draw your left-aligned grid
+    plot_left_aligned <- left_align(plot_name, c("subtitle", "title", "caption"))
+    plot_grid <- ggpubr::ggarrange(plot_left_aligned, footer, ncol = 1, nrow = 2, heights = c(1, 0.045/(height_pixels/450)))
+    ## print(paste('Saving to', save_filepath))
+    save_plot(plot_grid, width_pixels, height_pixels, save_filepath)
+    ## Return (invisibly) a copy of the graph. Can be assigned to a variable or silently ignored.
+    invisible(plot_grid)
 }
